e9932a7ed19fa5379b93ec9d063f3feb5abd0f98
nikkig124 <nicole.gibson@g.austincc.edu>
2020-01-27 19:05:59 -0600

    added tests and sqroot

--- src/examples/01_module/02_variables/vars.cpp
+++ src/examples/01_module/02_variables/vars.cpp
@@ -1,1 +1,15 @@
 //cpp
+
+#include "vars.h"
+
+
+void display_number(int num)
+{
+	cout << num << "\n";
+}
+
+int square_number(int num)
+{
+	
+	return num * num;
+}
--- test/examples_test/01_module_test/01_module_tests.cpp
+++ test/examples_test/01_module_test/01_module_tests.cpp
@@ -1,7 +1,15 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "vars.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+TEST_CASE("Square Number Funct Test") {
+	REQUIRE (square_number(0) == 0);
+	REQUIRE(square_number(5) == 25);
+	REQUIRE(square_number(-2) == 4);
+	//REQUIRE(square_number(-2) == 8);
 }
 
