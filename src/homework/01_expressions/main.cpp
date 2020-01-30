bd3de2688eb9a4078537c85d4d08220470cb1a2d
nikkig124 <nicole.gibson@g.austincc.edu>
2020-01-29 18:42:41 -0600

    HW1 complete

--- src/homework/01_expressions/main.cpp
+++ src/homework/01_expressions/main.cpp
@@ -5,17 +5,14 @@
 //write namespace using statement for cout
 using std::cout;
 
-
-
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
-	
-	cout << multiply_numbers(10,10) << "\n";
+	int result = multiply_numbers(10, 10);
 
-	cout << add_numbers(10, 10);
+	cout << "Result: "  << result;
 
 	return 0;
 }
